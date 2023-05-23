28c3a9aa475e17756976604283f9d240bbcaeed5
jinops <jinwoo.develop@gmail.com>
2023-05-23 11:13:00 +0900

    Add admin menu php

--- admin/admin_reservation.php
+++ admin/admin_reservation.php
@@ -5,8 +5,6 @@
   <title>Admin</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../admin/styles/style.css">
-  <link rel="stylesheet" href="styles/admin.css">
   <!-- for bootstrap library -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -17,27 +15,10 @@
 <body>
   <div class="row">
     <div class="col-2">
-      <ul id="menu" class="navbar-nav pt-3">
-        <h5 class="text-center">산공 스터디카페</h5>
-        <li class="p-3">
-          <a class="nav-link" href="./admin_notice.html">공지 사항</a>
-        </li>
-        <li class="p-3">
-          <a class="nav-link" href="./admin_user.html">회원 관리</a>
-        </li>
-        <li class="p-3">
-          <a class="nav-link" href="./admin_ticket.html">사용권 관리</a>
-        </li>
-        <li class="p-3">
-          <a class="nav-link" href="./admin_seat.html">좌석 관리</a>
-        </li>
-        <li class="p-3 selected">
-          <a class="nav-link" href="./admin_reservation.html">예약 내역</a>
-        </li>
-        <li class="p-3">
-          <a class="nav-link" href="./admin_payment.html">결제 내역</a>
-        </li>
-      </ul>
+      <?php 
+      $_GET['selected']='reservation';
+      include "menu.php" 
+      ?>
     </div>
     <div class="col-10 p-5">
       <h3>예약 내역</h3>
---
+++ admin/menu.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="styles/admin.css">
+
+<ul id="menu" class="navbar-nav pt-3">
+  <h5 class="text-center">산공 스터디카페</h5>
+  <li id="notice" class="p-3">
+    <a class="nav-link" href="./admin_notice.php">공지 사항</a>
+  </li>
+  <li id="user" class="p-3">
+    <a class="nav-link" href="./admin_user.php">회원 관리</a>
+  </li>
+  <li id="ticket" class="p-3">
+    <a class="nav-link" href="./admin_ticket.php">사용권 관리</a>
+  </li>
+  <li id="seat" class="p-3">
+    <a class="nav-link" href="./admin_seat.php">좌석 관리</a>
+  </li>
+  <li id="reservation" class="p-3">
+    <a class="nav-link" href="./admin_reservation.php">예약 내역</a>
+  </li>
+  <li id="payment" class="p-3">
+    <a class="nav-link" href="./admin_payment.php">결제 내역</a>
+  </li>
+</ul>
+
+<script>
+  function setSelectedMenu(menuId){
+    menu = document.getElementById(menuId);
+    menu.classList.add("selected");
+  }
+</script>
+
+<?php
+  $menu=$_GET['selected'];
+  echo "<script>setSelectedMenu('$menu')</script>"
+?>
