915851f23e0569da757926667fa3ea95c7925993
jinops <jinwoo.develop@gmail.com>
2023-05-26 14:34:32 +0900

    Update create_table.sql

--- query/create_table.sql
+++ query/create_table.sql
@@ -1,90 +1,87 @@
 /* author: sbbae, yhpark */
 
-create table p_notice(
-notice_id int auto_increment primary key,
-title varchar(20) null,
-content text null
+create table P_NOTICE(
+Notice_id int auto_increment primary key,
+Title varchar(20) null,
+Content text null
 );
 
-create table p_room(
-room_id int auto_increment primary key,
-r_name varchar(20) not null,
-r_width int not null,
-r_hight int not null
+create table P_ROOM(
+Room_id int auto_increment primary key,
+Name varchar(20) not null,
+Width int not null,
+Height int not null
 );
 
-
-
-
-create table p_seat( 
-seat_id int auto_increment primary key,
-room_id int not null,
-s_width int not null,
-s_hight int not null,
-x int not null,
-y int not null,
-constraint seat_FK foreign key(room_id)
-references p_room(room_id)
+create table P_SEAT( 
+Seat_id int auto_increment primary key,
+Room_id int not null,
+Width int not null,
+Height int not null,
+X int not null,
+Y int not null,
+constraint seat_FK foreign key(Room_id)
+references p_room(Room_Id)
 );
 
 
-create table p_user(
-user_id int auto_increment primary key,
-phone char(11) not null,
-password varchar(4) not null,
-name varchar(10) not null,
-total_payment varchar(30) not null
+create table P_USER(
+User_id int auto_increment primary key,
+Phone char(11) not null,
+Password varchar(4) not null,
+Name varchar(10) not null,
+Total_payment int null
 );
 
 
 
 
-
-create table p_reserve(
-res_id int auto_increment primary key,
-user_id int not null,
-room_id int not null,
-seat_id int not null,
-ticket_id int not null,
-start_time datetime not null,
-end_time datetime null,
-constraint res_userFK foreign key(user_id)
-   references p_user(user_id),
-constraint res_seatFK foreign key(room_id)
-   references p_room(room_id),
-constraint res_ticketFK foreign key(ticket_id)
-   references p_ticket(ticket_id)
+create table P_RESERVE(
+Reserve_Id int auto_increment primary key,
+User_id int not null,
+Room_id int not null,
+Seat_id int not null,
+Ticket_id int not null,
+Start_time datetime not null,
+End_time datetime null,
+constraint res_userFK foreign key(User_id)
+   references p_user(User_id),
+constraint res_seatFK foreign key(Room_id)
+   references p_room(Room_id),
+constraint res_ticketFK foreign key(Ticket_id)
+   references p_ticket(Ticket_id)
 );
 
 
-create table p_user_reserve(
-user_id int auto_increment not null,
-res_id int not null,
-constraint u_r_userFK foreign key(user_id)
-   references p_user(user_id),
-constraint u_r_resFK foreign key(res_id)
-   references p_reserve(res_id)
+create table P_USER_RESERVE(
+User_id int auto_increment not null,
+Reserve_id int not null,
+constraint u_r_userFK foreign key(User_id)
+   references p_user(User_id),
+constraint u_r_resFK foreign key(Reserve_id)
+   references p_reserve(Reserve_id)
 );
 
 
-create table p_payment(
-payment_id int auto_increment primary key,
-user_id int not null,
-ticket_id int not null,
-payment_price int not null,
-payment_time timestamp not null,
-payment_type varchar(10) not null,
-constraint p_userFK foreign key(user_id)
-   references p_user(user_id),
-constraint p_ticketFK foreign key(ticket_id)
-   references p_ticket(ticket_id),
-constraint payment_type_check check(payment_type in('신용카드', '현금'))
+create table P_PAYMENT(
+Payment_id int auto_increment primary key,
+User_id int not null,
+Ticket_id int not null,
+Price int not null,
+Time timestamp not null,
+Type varchar(10) not null,
+constraint p_userFK foreign key(User_id)
+   references p_user(User_id),
+constraint p_ticketFK foreign key(Ticket_id)
+   references p_ticket(Ticket_id),
+constraint payment_type_check check(Type in(card, cash))
 );
 
 
-create table p_ticket(
-ticket_id int auto_increment primary key,
-ticket_type varchar(20) not null,
-ticket_price int not null,
-duration_min int not null
+create table P_TICKET(
+Ticket_id int auto_increment primary key,
+Type varchar(20) not null,
+Price int not null,
+Duration_min int not null
 );
+
