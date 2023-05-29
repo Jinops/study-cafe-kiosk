bb1468aff890367fe1861a044bea7668bfc94ad2
jinops <jinwoo.develop@gmail.com>
2023-05-29 23:03:28 +0900

    Update test_data.sql

--- query/test_data.sql
+++ query/test_data.sql
@@ -3,6 +3,34 @@
 insert into P_NOTICE
 values (1, '공지사항1', '자리이동 및 소란스런 행위를 하시면 퇴실조치합니다.');
 
+insert into P_USER
+values (1, '01012345678', '1234', 'Carmen', 0);
+insert into P_USER
+values (2, '01022345678', '5678', 'Drew', 0);
+insert into P_USER
+values (3, '01032345678', '9101', 'Gordon', 0);
+insert into P_USER
+values (4, '01042345678', '1112', 'Aiden', 0);
+insert into P_USER
+values (5, '01052345678', '1314', 'Robin', 0);
+
+insert into P_TICKET
+values (1, '시간권', 2000, 120);
+insert into P_TICKET
+values (2, '시간권', 4000, 240);
+insert into P_TICKET
+values (3, '시간권', 6000, 360);
+insert into P_TICKET
+values (4, '시간권', 8000, 480);
+insert into P_TICKET
+values (5, '고정석', 50000, 10080);
+insert into P_TICKET
+values (6, '고정석', 100000, 20160);
+insert into P_TICKET
+values (7, '고정석', 150000, 30240);
+insert into P_TICKET
+values (8, '고정석', 210000, 43200);
+
 insert into P_ROOM
 values (1, '1 room', 50, 50);
 insert into P_ROOM
@@ -48,34 +76,6 @@ insert into P_SEAT
 values (19, 2, 20, 10, 40, 35);
 insert into P_SEAT
 values (20, 2, 20, 10, 40, 45);
-
-insert into P_TICKET
-values (1, '시간권', 2000, 120);
-insert into P_TICKET
-values (2, '시간권', 4000, 240);
-insert into P_TICKET
-values (3, '시간권', 6000, 360);
-insert into P_TICKET
-values (4, '시간권', 8000, 480);
-insert into P_TICKET
-values (5, '고정석', 50000, 10080);
-insert into P_TICKET
-values (6, '고정석', 100000, 20160);
-insert into P_TICKET
-values (7, '고정석', 150000, 30240);
-insert into P_TICKET
-values (8, '고정석', 210000, 43200);
-
-insert into P_USER
-values (1, '01012345678', '1234', 'Carmen', 0);
-insert into P_USER
-values (2, '01022345678', '5678', 'Drew', 0);
-insert into P_USER
-values (3, '01032345678', '9101', 'Gordon', 0);
-insert into P_USER
-values (4, '01042345678', '1112', 'Aiden', 0);
-insert into P_USER
-values (5, '01052345678', '1314', 'Robin', 0);
 
 insert into P_RESERVE
 values (1, 1, 1, 1, 1, '2023-05-28 19:31:17', '2023-05-28 21:31:17');
