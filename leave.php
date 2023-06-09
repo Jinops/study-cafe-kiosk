3bac88ca645bf559c0aceea520250ab105f35f28
jinops <jinwoo.develop@gmail.com>
2023-06-09 23:20:32 +0900

    Add case for fixed seat

--- leave.php
+++ leave.php
@@ -27,7 +27,7 @@ $reserve_id=$_GET['reserve_id'];
   <div class="container-sm center">
     <img src="images/logo.png" id="logo">
     <div class="justify-content-center border py-4 px-5 mx-md-3 text-center">
-      <h2 class="mb-4">퇴실하기</h2>
+      <h2 class="mb-4">사용중인 회원</h2>
       <hr />
       <h3>이미 사용중인 이용권이 있습니다.</h2>
       <div class="row text-center">
@@ -41,6 +41,7 @@ $reserve_id=$_GET['reserve_id'];
 
         $room_id = $row['Room_id'];
         $seat_id = $row['Seat_id'];
+        $ticket_id = $row['Ticket_id'];
         $start_time = $row['Start_time'];
         $end_time = $row['End_time'];
 
@@ -48,13 +49,34 @@ $reserve_id=$_GET['reserve_id'];
         echo"입실 시간: $start_time<br/>";
         echo"퇴실 예정 시간: $end_time</p>";
 
-        echo "
-        <a href='leave_process.php?reserve_id=$reserve_id'>
-          <button class='btn btn-xs btn-dark fs-1 btn_leave_type'>
-          <i class='bi bi-box-arrow-left'></i><br />퇴실하기
-          </button>
-        </a>
-        ";
+        $query_ticket = "SELECT Type FROM P_TICKET WHERE Ticket_id=$ticket_id";
+        $res = mysqli_query($mysqli, $query_ticket);
+        $rows_ticket = $res->fetch_all(MYSQLI_ASSOC);
+        $type = $rows_ticket[0]['Type'];
+
+        if ($type=='basic'){
+          echo "
+          <a href='leave_process.php?reserve_id=$reserve_id'>
+            <button class='btn btn-xs btn-dark fs-1 btn_leave_type'>
+            <i class='bi bi-box-arrow-left'></i><br />퇴실하기
+            </button>
+          </a>
+          ";
+        } else if ($type=='fixed'){
+          echo "
+          <h4>정기권 사용자 - 입실하세요</h4>
+          ";
+          echo "
+          <div>
+            <a href='./'><button class='btn btn-lg btn-dark'>
+              초기화면
+            </button></a>
+            <a href='leave_process.php?reserve_id=$reserve_id'><button class='btn btn-lg btn-secondary'>
+              퇴실
+            </button></a>
+          </div>
+          ";
+        }
       } catch (Exception $e){
         echo $query;
         echo $e;
